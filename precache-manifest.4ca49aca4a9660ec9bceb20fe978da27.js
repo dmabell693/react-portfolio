dd3f703e96023be5d94e00a3d1d6ea74656f2c29
Daniel Abell <da450@cairn.edu>
2020-06-30 17:50:56 -0400

    Updates

---
+++ precache-manifest.4ca49aca4a9660ec9bceb20fe978da27.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc80f11cc7306c4b96ba661c5e2e642c",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "e1ca2092bfad6289126d",
+    "url": "/react-portfolio/static/css/main.faa63f82.chunk.css"
+  },
+  {
+    "revision": "12d0b6c7467d256cd826",
+    "url": "/react-portfolio/static/js/2.6a3e31a4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-portfolio/static/js/2.6a3e31a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ca2092bfad6289126d",
+    "url": "/react-portfolio/static/js/main.1aaa73bc.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "75b827b2e9cdd161ee69186e8d85eac7",
+    "url": "/react-portfolio/static/media/Trales-and-Ales-screenshot.75b827b2.png"
+  },
+  {
+    "revision": "ce2928a5dc8df941589b077f280d172a",
+    "url": "/react-portfolio/static/media/burgernator-screenshot.ce2928a5.png"
+  },
+  {
+    "revision": "260e78247f8128b5c248804a86e7cd78",
+    "url": "/react-portfolio/static/media/employee-tracker-screenshot.260e7824.png"
+  },
+  {
+    "revision": "3d8a1c6768d002ab8f8cb56880aa0d4d",
+    "url": "/react-portfolio/static/media/hike-hire-screenshot.3d8a1c67.png"
+  },
+  {
+    "revision": "04026feb5534c1cb841ee86c52d93c76",
+    "url": "/react-portfolio/static/media/papyrus-dark.04026feb.png"
+  },
+  {
+    "revision": "72d54d3e5c3a5283d3b48d7a669fcb94",
+    "url": "/react-portfolio/static/media/profile-pic.72d54d3e.jpeg"
+  },
+  {
+    "revision": "ab735431c7a47204d63c0096a93975d6",
+    "url": "/react-portfolio/static/media/react-employee-directory-portfolio-screenshot.ab735431.png"
+  },
+  {
+    "revision": "d9506f91ad1e740be924af5bcd045ef3",
+    "url": "/react-portfolio/static/media/team-profile-generator-scrnsht-update.d9506f91.png"
+  }
+]);
