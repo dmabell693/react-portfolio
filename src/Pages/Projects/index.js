d15925dc92bebe143459b7df436f9a71fd7a2c8e
Daniel Abell <da450@cairn.edu>
2020-06-29 21:15:17 -0400

    converted projects.json to js file to successfully export image paths

--- src/Pages/Projects/index.js
+++ src/Pages/Projects/index.js
@@ -3,24 +3,8 @@ import PageWrapper from "../../components/PageWrapper";
 import PageHeader from "../../components/PageHeader";
 import ProjectCardWrapper from "../../components/ProjectCardWrapper";
 import ProjectCard from "../../components/ProjectCard";
-// import ProjectCardImages from "../../components/ProjectCardImages";
-import projects from "../../projects.json";
+import projects from "./projectData";
 import "./style.css";
-import directory from "./react-employee-directory-portfolio-screenshot.png";
-// import hikehire from "./hike-hire-screenshot.png";
-// import tralesales from "./Trales-and-Ales-screenshot.png";
-// import tracker from "./employee-tracker-screenshot.png";
-// import burgernator from "./burgernator-screenshot.png";
-// import generator from "./team-profile-generator-scrnsht-update.png";
-
-// const images = [
-//     { "id": 1, "image": directory },
-//     { "id": 2, "image": hikehire },
-//     { "id": 3, "image": tralesales },
-//     { "id": 4, "image": tracker },
-//     { "id": 5, "image": burgernator },
-//     { "id": 6, "image": generator }
-// ];
 
 function Projects() {
 
@@ -32,7 +16,7 @@ function Projects() {
                     <ProjectCard
                         key={project.id}
                         title={project.title}
-                        screenshot={directory}
+                        screenshot={project.screenshot}
                         deployedURL={project.deployedURL}
                         repoURL={project.repoURL}
                     />
--- src/Pages/Projects/projectData.js
+++ src/Pages/Projects/projectData.js
@@ -1,44 +1,54 @@
+import directory from "./images/react-employee-directory-portfolio-screenshot.png";
+import hikehire from "./images/hike-hire-screenshot.png";
+import tralesales from "./images/Trales-and-Ales-screenshot.png";
+import tracker from "./images/employee-tracker-screenshot.png";
+import burgernator from "./images/burgernator-screenshot.png";
+import generator from "./images/team-profile-generator-scrnsht-update.png";
+
+const projectData = 
 [
     {
         "id": 1,
         "title": "Employee Directory",
-        "screenshot": "../../react-employee-directory-portfolio.png",
+        "screenshot": directory,
         "deployedURL": "https://dmabell693.github.io/react-employee-directory/",
         "repoURL": "https://github.com/dmabell693/react-employee-directory"
     },
     {
         "id": 2,
         "title": "HikeHire",
-        "screenshot": "hikehire",
+        "screenshot": hikehire,
         "deployedURL": "https://shrouded-anchorage-20263.herokuapp.com/",
         "repoURL": "https://github.com/dmabell693/HikeHire"
     },
     {
         "id": 3,
         "title": "Trales & Ales",
-        "screenshot": "tralesales",
+        "screenshot": tralesales,
         "deployedURL": "https://dmabell693.github.io/Trales-and-Ales/",
         "repoURL": "https://github.com/dmabell693/Trales-and-Ales"
     },
     {
         "id": 4,
         "title": "Employee Tracker",
-        "screenshot": "tracker",
+        "screenshot": tracker,
         "deployedURL": "https://github.com/dmabell693/employee-tracker",
         "repoURL": "https://github.com/dmabell693/employee-tracker"
     },
     {
         "id": 5,
         "title": "The Burgernator",
-        "screenshot": "burgernator",
+        "screenshot": burgernator,
         "deployedURL": "https://fast-depths-87524.herokuapp.com/",
         "repoURL": "https://github.com/dmabell693/burgernator"
     },
     {
         "id": 6,
         "title": "Team Profile Generator",
-        "screenshot": "generator",
+        "screenshot": generator,
         "deployedURL": "https://github.com/dmabell693/team-profile-generator",
         "repoURL": "https://github.com/dmabell693/team-profile-generator"
     }
 ]
+
+export default projectData;
