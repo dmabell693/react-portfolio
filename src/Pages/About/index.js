1fa50c487185ca41e2b65966f19acd596176eafc
Daniel Abell <da450@cairn.edu>
2020-07-01 15:17:28 -0400

    added resume .pdf to About page

--- src/Pages/About/index.js
+++ src/Pages/About/index.js
@@ -3,6 +3,7 @@ import PageWrapper from "../../components/PageWrapper";
 import PageHeader from "../../components/PageHeader";
 import "./style.css";
 import image from "./profile-pic.jpeg";
+import resume from "./Abell_Resume.pdf";
 import { Link } from "react-router-dom";
 
 function About() {
@@ -22,7 +23,7 @@ function About() {
                                 <h6 className="card-title">Full-stack web developer. Richmond-raised Marine veteran. World traveller. Student of ancient languages.</h6>
                                 <h6 className="card-title"><a className="profile-link" href="https://github.com/dmabell693" target="_blank" rel="noopener noreferrer">Github</a></h6>
                                 <h6 className="card-title"><a className="profile-link" href="https://www.linkedin.com/in/daniel-abell-782350199/" target="_blank" rel="noopener noreferrer">LinkedIn</a></h6>
-                                <h6 className="card-title"><a className="profile-link" href="assets/images/Abell_Resume.pdf" target="_blank" rel="noopener noreferrer">Resume</a></h6>
+                                <h6 className="card-title"><a className="profile-link" href={resume} target="_blank" rel="noopener noreferrer">Resume</a></h6>
                                 <h6 className="card-title">
                                     email: <Link className="profile-link" to="/contact">dmabell693@gmail.com</Link>
                                 </h6>
