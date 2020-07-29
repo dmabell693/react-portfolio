bddb51bddf447d5283b29f7288fa393289e37589
Daniel Abell <da450@cairn.edu>
2020-07-28 20:42:56 -0400

    updated portfolio to reflect final project

--- src/Pages/Projects/projectData.js
+++ src/Pages/Projects/projectData.js
@@ -1,46 +1,46 @@
+import whatsgood from "./images/whats-good-screenshot-dkmode.png";
 import directory from "./images/react-employee-directory-portfolio-screenshot.png";
 import hikehire from "./images/hike-hire-screenshot.png";
 import tralesales from "./images/Trales-and-Ales-screenshot.png";
 import tracker from "./images/employee-tracker-screenshot.png";
-import burgernator from "./images/burgernator-screenshot.png";
 import generator from "./images/team-profile-generator-scrnsht-update.png";
 
 const projectData = 
 [
     {
         "id": 1,
+        "title": "What's Good",
+        "screenshot": whatsgood,
+        "deployedURL": "https://whatsgood-v1.herokuapp.com/",
+        "repoURL": "https://github.com/emijoha/Project3"
+    },
+    {
+        "id": 2,
         "title": "Employee Directory",
         "screenshot": directory,
         "deployedURL": "https://dmabell693.github.io/react-employee-directory/",
         "repoURL": "https://github.com/dmabell693/react-employee-directory"
     },
     {
-        "id": 2,
+        "id": 3,
         "title": "HikeHire",
         "screenshot": hikehire,
         "deployedURL": "https://shrouded-anchorage-20263.herokuapp.com/",
         "repoURL": "https://github.com/dmabell693/HikeHire"
     },
     {
-        "id": 3,
+        "id": 4,
         "title": "Trales & Ales",
         "screenshot": tralesales,
         "deployedURL": "https://dmabell693.github.io/Trales-and-Ales/",
         "repoURL": "https://github.com/dmabell693/Trales-and-Ales"
     },
     {
-        "id": 4,
+        "id": 5,
         "title": "Employee Tracker",
         "screenshot": tracker,
         "deployedURL": "https://github.com/dmabell693/employee-tracker",
         "repoURL": "https://github.com/dmabell693/employee-tracker"
-    },
-    {
-        "id": 5,
-        "title": "The Burgernator",
-        "screenshot": burgernator,
-        "deployedURL": "https://fast-depths-87524.herokuapp.com/",
-        "repoURL": "https://github.com/dmabell693/burgernator"
     },
     {
         "id": 6,
